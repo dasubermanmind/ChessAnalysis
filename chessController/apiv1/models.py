2f969782de7fe21fbe7c1dddab8a004444dab5d3
EOstling <it.treugott@gmail.com>
2021-04-15 07:14:45 -0400

    Small update

--- chessController/apiv1/models.py
+++ chessController/apiv1/models.py
@@ -4,8 +4,8 @@ class GameOpenings(models.Model):
     openingName = models.CharField(max_length=900)
     games = models.CharField(max_length=900)
     openingEco = models.CharField(max_length=900)
-    whiteRating=models.IntegerField()
+    whiteRating = models.IntegerField()
     blackRating = models.IntegerField()
     winner = models.CharField(max_length=900)
     totalStrength = models.IntegerField()
-#end
+# end
--- chessController/apiv1/views.py
+++ chessController/apiv1/views.py
@@ -9,41 +9,56 @@ from .serializers import GameOpeningsSerializers
 from .serializers import CreateGameOpeningSerializer
 from .models import GameOpenings
 
-class GameOpeningView(generics.CreateAPIView):
-    querset = GameOpenings.objects.all()
-    serializer_class = GameOpeningsSerializers
-#end
 
-class CreateGameOpeningView(APIView):
+class GameOpeningView(generics.CreateAPIView):
+    queryset = GameOpenings.objects.all()
+    serializer_class = GameOpeningsSerializers
+
+
+class CreateGameOpeningView(generics.UpdateAPIView):
+    queryset = GameOpenings.objects.all()
+    serializer_class = CreateGameOpeningSerializer
+
+
+class UpdateAPIView(APIView):
     serializer = CreateGameOpeningSerializer
 
-    def post(self,request,format=None):
-        if not self.request.session.exists(self.requet.session.session_key):
-            #create the session
+    def post(self, request):
+        if not self.request.session.exists(self.request.session.session_key):
+            # create the session
             self.request.session.create()
-    
-        serializer = self.serializer_class(data=request.data)
+
+        serializer = self.serializer(data=request.data)
         if serializer.is_valid():
-            openingName  = serializer.data.openingName
-            winner = self.request.session.session_key #this is what we are updating in relation to
-            whiteRating= serializer.data.whiteRating
+            openingName = serializer.data.openingName
+            winner = serializer.data.winner
+            whiteRating = serializer.data.whiteRating
             blackRating = serializer.data.blackRating
-            querySet = GameOpenings.objects.filter(winner=winner)
+            games = serializer.data.games
+            totalStrength = serializer.data.totalStrength
+            openingEco = serializer.data.openingEco
+
+            querySet = GameOpenings.objects.create(openingName, winner, whiteRating, blackRating, games, totalStrength,
+                                                   openingEco)
             if querySet.exists():
                 gameOpening = queryset[0]
                 gameOpening.winner = winner
                 gameOpening.whiteRating = whiteRating
                 gameOpening.blackRating = blackRating
+                gameOpening.openingName = openingName
 
-                gameOpening.save(update_fields=['winner', 'whiteRating','blackRating','openingName'])
+                gameOpening.save(
+                    update_fields=['openingName', 'games', 'openingEco', 'whiteRating', 'blackRating', 'winner',
+                                   'totalStrength'])
                 self.request.session['openingName'] = gameOpening.openingName
                 return Response(CreateGameOpeningSerializer(gameOpening).data, status=status.HTTP_200_OK)
             else:
-                gameOpening = GameOpenings(openingName,winner,whiteRating,blackRating)
+                gameOpening = GameOpenings(openingName, games, openingEco, whiteRating, blackRating, winner,
+                                           totalStrength)
                 gameOpening.save()
+
                 self.request.session['openingName'] = gameOpening.openingName
                 return Response(CreateGameOpeningSerializer(gameOpening).data, status=status.HTTP_201_CREATED)
 
         return Response({'Bad Request': 'Invalid data...'}, status=status.HTTP_400_BAD_REQUEST)
-#end
-
+# end
